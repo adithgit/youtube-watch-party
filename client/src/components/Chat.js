5e6c5681ede1c1c6ef343f0572eb1b92a8246763
adithgit <5285@mbcpeermade.com>
2022-06-15 22:57:51 +0530

    bug fix chat section

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,47 +1,59 @@
 import React from 'react';
 import './Room.css';
 import { TextField } from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import FilledInput from '@mui/material/FilledInput';
+import SendIcon from '@mui/icons-material/Send';
 import { Send } from '@mui/icons-material';
-import InputAdornment from '@mui/material';
-import IconButton from '@mui/material';
 function Chat() {
   const members = [
     '12212',
-    'qww','12212',
-    'qww','12212',
-    'qww','12212',
-    'qww','12212',
-    'qww','12212',
-    'qww','12212',
-    'qww','12212',
+    'qww', '12212',
+    'qww', '12212',
+    'qww', '12212',
+    'qww', '12212',
+    'qww', '12212',
+    'qww', '12212',
+    'qww', '12212',
     'qww'
   ]
   return (
     <div className='chat-container'>
-     <div className='chat-field'>
-       <div className='member-field'>
-        {
-          members.map((member)=>{
-            return <span className='member'>
-            {member}
-            </span>
-          })
-        }
-       </div>
-       <div className='text-field'>
+      <div className='chat-field'>
+        <div className='member-field'>
+          {
+            members.map((member) => {
+              return <span className='member'>
+                {member}
+              </span>
+            })
+          }
+        </div>
+        <div className='text-field'>
 
-       </div>
-       <div className='input-field'>
-       <TextField
-       fullWidth
-  hiddenLabel
-  id="filled-hidden-label-normal"
-  defaultValue="Send a message"
-  variant="filled"
-  color='white'
-/>
-       </div>
-     </div>
+        </div>
+        <div className='input-field'>
+          <FormControl fullWidth variant="filled">
+            <InputLabel htmlFor="filled-hidden-label-normal">Enter message to send</InputLabel>
+            <FilledInput
+              id="filled-hidden-label-normal"
+              color='white'
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    edge="end"
+                  >
+                    <SendIcon color='white' />
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+        </div>
+      </div>
     </div>
   )
 }
