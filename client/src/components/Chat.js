09c3c1157fa89b1675b4706ae0ad52ad55fc5e88
adithgit <5285@mbcpeermade.com>
2022-06-18 23:01:41 +0530

    Frontend completed

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
 import './Room.css';
-import FormControl  from '@mui/material/FormControl';
+import FormControl from '@mui/material/FormControl';
 import InputAdornment from '@mui/material/InputAdornment';
-import  Input from '@mui/material/Input';
-import IconButton  from '@mui/material/IconButton';
-import  Send  from '@mui/icons-material/Send';
+import Input from '@mui/material/Input';
+import IconButton from '@mui/material/IconButton';
+import Send from '@mui/icons-material/Send';
 import { useEffect, useRef } from 'react';
-
-
 function Chat() {
   const chatBox = useRef();
+  const textField = useRef();
 
   const members = [
-    '12212',
+    '12212', 
     'qww', '12212',
     'qww', '12212',
     'qww', '12212',
@@ -25,31 +24,26 @@ function Chat() {
   const [chat, setChat] = useState([]);
 
   const chatData = [
-    {name:'name1',message:'Hey there'},
-    {name:'aduth',message:'asdfh therer adsithya how are you doing.'},
-    {name:'nothing much',message:'oh okey'},
-    {name:'name1',message:'Hey there'},
-    {name:'aduth',message:'asdfh therer adsithya how are you doing.'},
-    {name:'nothing much',message:'oh okey'},
-    {name:'name1',message:'Hey there'},
-    {name:'aduth',message:'asdfh therer adsithya how are you doing.'},
-    {name:'nothing much',message:'oh okey'},
-    {name:'name1',message:'Hey there'},
-    {name:'aduth',message:'asdfh therer adsithya how are you doing.'},
-    {name:'nothing much',message:'oh okey'},
+    { name: 'name1', message: 'Hey there' },
   ]
 
-useEffect(()=>{
-  setChat( chatData )
-},[])
-const sendMessage = ()=>{
-  setChat( [...chat, {name:null, message:chatBox.current.value}] )
-}
+  useEffect(() => {
+    setChat(chatData)
+    // Get the scroll height of textField and scroll down using top option of scroll method along with smooth behavior
+
+    textField.current.addEventListener('DOMNodeInserted', event => {
+      const { currentTarget: target } = event;
+      target.scroll({ top: target.scrollHeight, behavior: 'smooth' });
+    });
+  }, [])
+  const sendMessage = () => {
+    setChat([...chat, { name: null, message: chatBox.current.value }])
+  }
   return (
     <div className='chat-container'>
       <div className='chat-field'>
         <div className='member-field'>
-        <span style={{marginRight:'1rem'}}>MEMBERS</span>
+          <span style={{ marginRight: '1rem' }}>MEMBERS</span>
           {
             members.map((member) => {
               return <span className='member'>
@@ -58,32 +52,32 @@ const sendMessage = ()=>{
             })
           }
         </div>
-        <div className='text-field'>
-        {
-          chat.map(( message )=>{
-            return message.name ? <div className='left-message'><span>{message.name +" : "+message.message}</span></div>:
-            <div className='right-message'><span>{message.message}</span></div>; 
-          })
-        }
+        <div className='text-field' ref={textField}>
+          {
+            chat.map((message) => {
+              return message.name ? <div className='left-message'><div className="name">{message.name}</div><div className="message">{message.message}</div></div> :
+                <div className='right-message'><div className="message">{message.message}</div></div>;
+            })
+          }
         </div>
         <div className='input-field'>
-        <FormControl  id='filled-basic'  variant='filled' sx={{ width:'90%', input: { color: 'white' }}} >
-          <Input
-          placeholder='Enter your message here'
-          inputRef={ chatBox }
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  edge="end"
-                  color='primary'
-                >
-                 <Send  sx={{ color: 'white'}} onClick={ sendMessage }  />
-                </IconButton>
-              </InputAdornment>
-            }
-          />
-        </FormControl>
+            <FormControl id='filled-basic' variant='filled' sx={{ width: '90%', input: { color: 'white' } }} >
+            <Input
+              placeholder='Enter your message here'
+              inputRef={chatBox}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    edge="end"
+                    color='primary'
+                  >
+                    <Send sx={{ color: 'white' }} onClick={sendMessage} />
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
         </div>
       </div>
     </div>
