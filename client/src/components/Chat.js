848cfb8c76be6bf150b90989276db810dc145fba
adithgit <5285@mbcpeermade.com>
2022-06-26 20:58:51 +0530

    Toast when user joins or leaves.

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -9,38 +9,45 @@ import { useEffect, useRef } from 'react';
 import { socketContext } from './App';
 import { userContext } from './UserContext';
 import { useParams } from 'react-router-dom';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
 
 function Chat() {
   const paramObj = useParams();
   const chatBox = useRef();
   const textField = useRef();
-  const user = useContext( userContext );
-  const socket = useContext( socketContext );
+  const user = useContext(userContext);
+  const socket = useContext(socketContext);
   const [chat, setChat] = useState([]);
+  const [open, setOpen] = useState({message:'Something went wrong.', state: false });
 
   // Update user list when a user joins the room
-  socket.on('user-list',( data )=>{
-    console.log(data);
-    user.updateUsers( data )
+  socket.on('user-list', (data) => {
+    console.log('User list updated.');
+    user.updateUsers(data);
+    setOpen({message:`${data[data.length-1].name} joined the room.`,state:true});
   })
 
-  socket.on('message-recieve', ( data )=>{
-    if( data.name === socket.userName ) return;
-    setChat([ ...chat, data ])
+  socket.on('message-recieve', (data) => {
+    if (data.name === socket.userName) return;
+    setChat([...chat, data]);
   })
 
-  socket.on('closed', ( socketId )=>{
-    user.updateUsers( user.users.filter((socket)=>{
-      return socket.id != socketId
-    }) );
+  socket.on('closed', (socketId) => {
+    user.updateUsers(user.users.filter((socket) => {
+      if( socket.id != socketId ){
+        return socket
+      }
+      setOpen({ message: `${socket.name} left the room`, state: true });
+    }));
   })
 
   useEffect(() => {
-    async function fetchUsers(){
+    async function fetchUsers() {
       console.log(paramObj.roomId);
       const response = await fetch(`http://localhost:3002/getUsers/${paramObj.roomId}`);
       const responseData = await response.json();
-      user.updateUsers( responseData );
+      user.updateUsers(responseData);
       console.log("Users fetched succesfullly");
     }
     fetchUsers();
@@ -55,13 +62,22 @@ function Chat() {
   const sendMessage = () => {
     console.log(socket.roomId);
     const message = chatBox.current.value;
-    socket.emit( 'message-sent', {name: socket.userName, message, roomId: socket.roomId });
+    socket.emit('message-sent', { name: socket.userName, message, roomId: socket.roomId });
     console.log('message sent');
     setChat([...chat, { name: null, message: chatBox.current.value }])
   }
 
+  const handleClose = () => {
+    setOpen({state:false});
+  }
+
   return (
     <div className='chat-container'>
+      <Snackbar open={open.state} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="info" sx={{ width: '100%' }}>
+          {open.message}
+        </Alert>
+      </Snackbar>
       <div className='chat-field'>
         <div className='member-field'>
           <span style={{ marginRight: '1rem' }}>MEMBERS</span>
@@ -82,7 +98,7 @@ function Chat() {
           }
         </div>
         <div className='input-field'>
-            <FormControl id='filled-basic' variant='filled' sx={{ width: '90%', input: { color: 'white' } }} >
+          <FormControl id='filled-basic' variant='filled' sx={{ width: '90%', input: { color: 'white' } }} >
             <Input
               placeholder='Enter your message here'
               inputRef={chatBox}
--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -9,22 +9,22 @@ function Player() {
   const urlParams = useParams();
   const videoState = useContext(videoContext);
 
-  const [loading , setLoading] = useState(true)
+  const [loading , setLoading] = useState('hidden')
   console.log("This is video state " + videoState.videoId);
+  
+  const onReady = (event) => {
+    console.log("This called");
+    setLoading('block')
+  }
+  
   let opts = {
     playerVars: {
-      autoplay: 1,
+      autoplay: 0,
     },
   }
-
-  const onReady = (event) => {
-    event.target.pauseVideo();
-    setLoading(false)
-  }
-
   return (
     <div className='youtube-container'>
-     <YouTube className='iframe-container' loading='eager' videoId={videoState.videoId} opts={opts} onReady={onReady} />
+     <YouTube  className='iframe-container' style={{display:{loading}}} videoId={videoState.videoId} opts={opts}  onReady={setLoading} />
       <div className='next-video'>
         <DialogButton title='Change video' dialog='Enter the link to the youtube video.' label='YouTube link' readOnly={false} />
         <DialogButton title='Share RoomID' dialog='Copy RoomID and share it.' label='RoomID' noChange readOnly={true} roomId={urlParams.roomId} />
